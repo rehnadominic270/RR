9da88712861642b5be67b526dc3a72426e2a432f
rehnadominic270 <rehnadominic270@gmail.com>
2021-05-05 09:34:06 -0500

    First commit

---
+++ src/codes/DiffOfTwoNumbers.java
@@ -0,0 +1,12 @@
+package codes;
+
+public class DiffOfTwoNumbers {
+
+	public static void main(String[] args) {
+		
+		int a=60;
+		int b=10;
+		System.out.println("The difference is "+(a-b));
+	}
+
+}
---
+++ src/codes/SumOfTwoNumbers.java
@@ -0,0 +1,15 @@
+package codes;
+
+public class SumOfTwoNumbers {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		
+		System.out.println("********************");
+		System.out.println("The sum is "+(a+b));
+		System.out.println("********************");
+
+	}
+
+}
